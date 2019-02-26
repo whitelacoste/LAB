194c565455b9745f397ad562862eb48faa8bb4c9
Antichrist <47594905+whitelacoste@users.noreply.github.com>
2019-02-27 02:04:13 +0300

    добавили вычитание

--- main.c
+++ main.c
@@ -4,8 +4,7 @@ int main(){
     int a, b;
     printf("Введите числа: ");
     scanf("%d %d",&a,&b);
-
-
+    
     printf("Сумма чисел: %d", a+b);
     printf("Разность чисел: %d", a-b);
     return 0;
